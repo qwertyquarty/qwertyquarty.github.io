6a0003709bd7299b33546445b74b5e239b305e76
qwertyquarty <qwertyquarty@gmail.com>
2024-12-10 21:18:13 +0100

    Fixed the title animation

--- anim.js
+++ anim.js
@@ -46,13 +46,13 @@ let anim2 = [
 let anim1Reverse = [...anim1].reverse();
 let anim2Reverse = [...anim2].reverse();
 
-anim1 = anim1.concat(anim1Reverse);
-anim2 = anim2.concat(anim2Reverse);
+anim1 = anim1.concat(anim1Reverse.slice(1));
+anim2 = anim2.concat(anim2Reverse.slice(1));
 
 let i = 0;
 
 setInterval(() => {
   document.title = `${anim1[i]} qwertyquarty ${anim2[i]}`;
-  i %= anim1.length
+  i %= (anim1.length - 1)
   i++;
 }, 500);
